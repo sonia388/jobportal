25536ad710430677ae73867f7248b1a980a0c546
sonia <sonia567@gmail.com>
2025-09-29 06:57:49 +0530

    get

--- frontend/src/components/JobDescription.jsx
+++ frontend/src/components/JobDescription.jsx
@@ -3,7 +3,7 @@ import { Badge } from './ui/badge'
 import { Button } from './ui/button'
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
-import { APPLICATION_API_END_POINT, JOB_API_END_POINT } from '@/utils/constant';
+import { APPLICATION_API_END_POINT,JOB_API_END_POINT } from '@/utils/constant';
 import { setSingleJob } from '@/redux/jobSlice';
 import { useDispatch, useSelector } from 'react-redux';
 import { toast } from 'sonner';
@@ -20,7 +20,7 @@ const JobDescription = () => {
 
     const applyJobHandler = async () => {
         try {
-            const res = await axios.get(`https://jobportal-1-px6m.onrender.com/api/v1/job/apply/${jobId}`, { withCredentials: true });
+            const res = await axios.get(`https://jobportal-1-px6m.onrender.com/api/v1/application/apply/${jobId}`, { withCredentials: true });
 
             if (res.data.success) {
                 setIsApplied(true); // Update the local state
